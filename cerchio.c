19ef1b84b5be0edf064f41ae9d827979abc47000
Cristian-alfa <34458827+Cristian-alfa@users.noreply.github.com>
2018-01-29 18:23:44 +0100

    Add files via upload

---
+++ cerchio.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+	int r;
+	float p, c, a;
+	//constante
+	p = 3.14;
+	//variabile
+	printf ("inserisci il raggio: ");
+	scanf ("%d", &r);
+	//operazioni
+	c = 2* p * r;
+	a = p* r* r;
+	//output
+	printf ("La circonferenza del cerchio di raggio %d e': %f\n", r, c);
+	printf ("La superficio del cerchio di raggio %d e': %f\n", r, a);
+
+	return 0;
+}	
