bba9510c74d1e4a1345bb45ccf8cabf9162e08fa
Cristian-alfa <krisian.m01@gmail.com>
2018-02-05 12:03:56 +0100

    somma_cifre.c, input file singolo

--- somma_cifre.c
+++ somma_cifre.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+
 int main ()
 {
-	int a, b, c, s;
+	int a, s, x, y, z;
 	//variabili
 	printf ("Inserire un numero formato da 3 cifre: ");
-	scanf ("%d%d%d", &a, &b, &c);
+	scanf ("%d", &a);
 	//somma
-	s = a + b + c;
-	printf ("La somma delle 3 cifre e': %d", s);
+  x = a % 10;
+  a/=10;
+  y = a % 10;
+  a/=10;
+  z = a % 10;
+  s = x + y + z;
+	printf ("La somma delle 3 cifre e': %d\n", s);
 
 	return 0;
 }
